47ddaf49158ee03600ac097147abf0cfb20a46bc
priyanshuSharma-WebDev <priyanshushrama709@gmail.com>
2022-03-07 21:42:04 +0530

    add toogle switch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,21 @@ function App() {
 
 
   const [animate, setAnimate] = useState(false);
+  const [isOn, setIsOn] = useState(false);
 
   const variants = {
     open: { opacity: 1, x: 0 },
     closed: { opacity: 0, x: "-100%" }
+  };
+
+
+  const toggleSwitch = () => setIsOn(!isOn);
+
+
+  const spring = {
+    type: "spring",
+    stiffness: 700,
+    damping: 30
   };
 
   const now = new Date()
@@ -28,38 +39,38 @@ function App() {
             x: { type: "tween" },
             default: { duration: 0.6 }
           }}
-           className={`${animate ? "pointer-events-auto": "pointer-events-none"} fixed top-16 left-2 w-56 normal:w-64 sm:w-72 bg-secondaryBackgroundDarkColor border-2 border-solid border-borderColor bottom-4 rounded-2xl shadow-xl flex flex-col justify-between`}
+          className={`${animate ? "pointer-events-auto" : "pointer-events-none"} fixed top-16 left-2 w-56 normal:w-64 sm:w-72 bg-secondaryBackgroundDarkColor border-2 border-solid border-borderColor bottom-4 rounded-2xl shadow-xl flex flex-col justify-between`}
         >
           <div>
-              <div className='flex items-center justify-center mt-6 pb-6 border-b-2 border-solid border-b-borderColor'>
-                <p className='text-sm text-secondaryTextColor'>{date.format(now, 'ddd, MMM DD YYYY')}</p>
-              </div>
-
-              <ul className='w-full mt-4 '>
-                <li className='text-lg h-12 w-11/12 cursor-pointer mx-auto flex items-center justify-start transition-all hover:border-transparent pl-4 rounded-lg hover:bg-mainButtonColor/10'>
-                  <IconContext.Provider value={{ className: "text-secondaryTextColor mr-3 w-4 h-4" }}>
-                    <FiGlobe />
-                  </IconContext.Provider>
-                  <span className='text-base'>Explore</span>
-                </li>
-                <li className='text-lg h-12 w-11/12 cursor-pointer mx-auto flex items-center justify-start transition-all mb-2 hover:border-transparent pl-4 rounded-lg hover:bg-mainButtonColor/10'>
-                  <IconContext.Provider value={{ className: "text-secondaryTextColor mr-3 w-4 h-4" }}>
-                    <FiFolder />
-                  </IconContext.Provider>
-                  <span className='text-base'>Your Work</span>
-                </li>
-              </ul>
+            <div className='flex items-center justify-center mt-6 pb-6 border-b-2 border-solid border-b-borderColor'>
+              <p className='text-sm text-secondaryTextColor'>{date.format(now, 'ddd, MMM DD YYYY')}</p>
             </div>
-            <footer className='flex items-center justify-center mb-8 text-tiny text-secondaryTextColor'>
-              Made with 💜 in india
-            </footer>
+
+            <ul className='w-full mt-4 '>
+              <li className='text-lg h-12 w-11/12 cursor-pointer mx-auto flex items-center justify-start transition-all hover:border-transparent pl-4 rounded-lg hover:bg-mainButtonColor/10'>
+                <IconContext.Provider value={{ className: "text-secondaryTextColor mr-3 w-4 h-4" }}>
+                  <FiGlobe />
+                </IconContext.Provider>
+                <span className='text-base'>Explore</span>
+              </li>
+              <li className='text-lg h-12 w-11/12 cursor-pointer mx-auto flex items-center justify-start transition-all mb-2 hover:border-transparent pl-4 rounded-lg hover:bg-mainButtonColor/10'>
+                <IconContext.Provider value={{ className: "text-secondaryTextColor mr-3 w-4 h-4" }}>
+                  <FiFolder />
+                </IconContext.Provider>
+                <span className='text-base'>Your Work</span>
+              </li>
+            </ul>
+          </div>
+          <footer className='flex items-center justify-center mb-8 text-tiny text-secondaryTextColor'>
+            Made with 💜 in india
+          </footer>
         </motion.div>
         <div className='flex h-full items-center justify-center'>
           <button onClick={() => {
             setAnimate(!animate)
           }}>
             <IconContext.Provider value={{ className: "text-mainButtonColor mr-3 w-6 h-6" }}>
-              {animate ? <FiX /> :<FiMenu />}
+              {animate ? <FiX /> : <FiMenu />}
             </IconContext.Provider>
           </button>
           <div className='nav-logo__custom-style__small w-7 h-7 rounded-md bg-gradient-to-tr from-[#f977ce] to-[#c373f2]  mr-2'>
@@ -68,9 +79,13 @@ function App() {
         </div>
         <a className='text-mainAccentColor underline' href="http://" target="_blank" rel="noopener noreferrer">explore</a>
       </nav>
-      <section className='mt-16'>
-        <h1>Or, you can explore other
+      <section className='mt-28 w-4/5 mx-auto flex flex-col justify-between items-center'>
+        <h1 className='mb-8 text-base text-center'>Or, you can explore other
           people projects and threads.</h1>
+        <div className="switch w-48 h-20 bg-accentDarkBg flex justify-start rounded-3xl cursor-pointer" data-isOn={isOn} onClick={toggleSwitch}>
+          <motion.div className={`handle w-28 h-20 bg-mainButtonColor rounded-3xl relative ${isOn ? "before:content-['Threads']" : "before:content-['Projects']"} 
+          before:absolute before:top-1/2 before:left-1/2 before:text-mainBackgroundDarkColor before:transform before:-translate-x-1/2 before:-translate-y-1/2`} layout transition={spring} />
+        </div>
       </section>
       <table>
         <thead>
