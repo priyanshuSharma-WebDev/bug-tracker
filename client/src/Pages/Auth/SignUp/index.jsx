97d390d956570f7650640aa7917fadb52bdfc622
priyanshuSharma-WebDev <priyanshushrama709@gmail.com>
2022-04-04 23:51:41 +0530

    style input fields

--- client/src/Pages/Auth/SignUp/index.jsx
+++ client/src/Pages/Auth/SignUp/index.jsx
@@ -8,12 +8,12 @@ function SignUp() {
         <h1 className='text-3xl font-black mb-3'>Welcome 🤗</h1>
         <p className='text-xl leading-5 text-mainTextColor'><p>Let's Sign you Up,</p>We will love to working with you.</p>
       </div>
-      <form className='bg-danger '>
-        <div className='bg-mainAccentColor w-fit'>
-          <label htmlFor="name"><input id="name" type="text" /></label>
-          <label htmlFor="email"><input id="email" type="email" /></label>
-          <label htmlFor="password"><input id="password" type="password" /></label>
-          <label htmlFor="password"><input id="password" type="password" /></label>
+      <form className='SignUp-form'>
+        <div className=' w-fit flex flex-col'>
+          <label  htmlFor="name"><input id="name" className='h-16 mb-2 w-60 rounded-xl bg-secondaryBackgroundDarkColor border-2 border-solid border-borderColor text-lg pl-6 outline-none' placeholder='Name - eg. Jhon Doe' type="text" /></label>
+          <label htmlFor="email"><input id="email" className='h-16 mb-2 w-60 rounded-xl bg-secondaryBackgroundDarkColor border-2 border-solid border-borderColor text-lg pl-6 outline-none' type="email" placeholder='Email - eg.jhonDoe@gmail.com' /></label>
+          <label htmlFor="password"><input id="password" className='h-16 mb-2 w-60 rounded-xl bg-secondaryBackgroundDarkColor border-2 border-solid border-borderColor text-lg pl-6 outline-none' type="password" placeholder='Password - eg. ljfw3094wojf' /></label>
+          <label htmlFor="password"><input id="password" className='h-16 mb-2 w-60 rounded-xl bg-secondaryBackgroundDarkColor border-2 border-solid border-borderColor text-lg pl-6 outline-none' type="password"  placeholder='Password - eg. ljfw3094wojf'/></label>
         </div>
         <button type='submit'>Submit</button>
       </form>
