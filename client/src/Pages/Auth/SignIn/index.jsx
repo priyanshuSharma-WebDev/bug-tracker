c447682e71319383f4ca0a117c3bec789fcc051c
priyanshuSharma-WebDev <priyanshushrama709@gmail.com>
2022-04-05 22:57:45 +0530

    do some fixes

--- client/src/Pages/Auth/SignIn/index.jsx
+++ client/src/Pages/Auth/SignIn/index.jsx
@@ -14,7 +14,7 @@ function SignIn() {
             <label htmlFor="username-email"><input id="username-email" className='h-16 normal:h-20 mb-2 w-64 normal:w-72 semiNormal:w-80 md:w-96 rounded-xl bg-secondaryBackgroundDarkColor  text-lg pl-6 pr-3 outline-none' placeholder='@jhonDoe - jhonDoe@gmail.com' type="text" /></label>
             <label htmlFor="password"><input id="password" className='h-16 normal:h-20 mb-2 w-64 normal:w-72 semiNormal:w-80 md:w-96 rounded-xl bg-secondaryBackgroundDarkColor  text-lg pl-6 outline-none' type="password" placeholder='Password - eg. ljfw3094wojf' /></label>
         </div>
-        <Link to="/Reset_Password" className='mt-2 text-secondaryTextColor mr-3 cursor-pointer'>Forgot Password?</Link>
+        <Link to="/Reset_Password" className='mt-0 text-secondaryTextColor mr-3 cursor-pointer'>Forgot Password?</Link>
         <div className='flex flex-col  items-center justify-center mt-6 w-full'>
           <button type='submit' className='h-16 normal:h-20 text-mainBackgroundDarkColor w-52 normal:w-56 mb-4 bg-mainButtonColor rounded-xl border-none font-bold shadow-lg cursor-pointer transition duration-300 text-lg'>Sign In</button>
           <p className='text-secondaryTextColor text-tiny'>Don't have an account? <Link className='text-mainAccentColor underline cursor-pointer' to="/SignUp">SignUp</Link></p>
